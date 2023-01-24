3fcca432266d9faf75dac810a6faacd92f0fa933
Olga Kusakina <kusakina_olya@mail.ru>
2023-01-24 14:44:21 +0300

    add page, data, test

---
+++ src/test/java/ru/netology/data/DbUtils/PaymentEntity.java
@@ -0,0 +1,19 @@
+package ru.netology.data.DbUtils;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentEntity {
+
+    private String id;
+    private String amount;
+    private String created;
+    private String status;
+    private String transactions_id;
+
+}
---
+++ src/test/java/ru/netology/test/TestCard.java
@@ -0,0 +1,64 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.DbHelper;
+import ru.netology.page.BuyByCard;
+import ru.netology.page.BuyByCredit;
+import ru.netology.page.HomePage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.*;
+import static ru.netology.data.DbHelper.*;
+
+
+public class TestCard {
+
+    DataHelper.CardNumber approvedCard = DataHelper.approvedCardInfo();
+    DataHelper.CardNumber declinedCard = DataHelper.declinedCardInfo();
+
+    @BeforeEach
+    public void cleanTables() {
+        DbHelper.cleanData();
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setupClass() {
+        open("http://localhost:8080");
+    }
+
+    @Test
+    void shouldSwitchBetweenPages() {
+        HomePage homePage = new HomePage();
+        BuyByCredit buyByCredit = homePage.getPageCredit();
+        BuyByCard buyByCard = buyByCredit.buyByCard();
+        buyByCard.buyByCreditCard();
+    }
+
+    @Test
+    void shouldBuyByApprovedCard() {
+        HomePage homePage = new HomePage();
+        BuyByCard buyByCard = homePage.getPageByCard();
+        buyByCard.enterCardData(getApprovedCardInfo(), getValidMonth(), getValidYear(), getValidOwner(), getValidCvc());
+        buyByCard.getSuccessMessage();
+        assertEquals(approvedCard.getStatus(), payData().getStatus());
+    }
+
+}
+
